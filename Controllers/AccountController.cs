724d34ce2cbf95a1c8157c76222cfdf939f308b8
Supriya Hegde <shegde@dovercorp.com>
2021-10-18 10:10:05 +0530

    first commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using coreSessionManagementApplication.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Session;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using coreSessionManagementApplication.Models;
+
+namespace coreSessionManagementApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        ApplicationDBContext context;
+        public AccountController()
+        {
+            context = new ApplicationDBContext();
+        }
+
+        public object Context { get; private set; }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            var userObj = context.Users.Where(u => u.Username == user.Username 
+            && u.Password == user.Password
+            ).SingleOrDefault();
+            if (userObj != null)
+            {
+                SessionHelper.setObjectAsJson(HttpContext.Session, "user", userObj);
+                User usr = SessionHelper.GetObjectFromJson<User>(HttpContext.Session, "user");
+                HttpContext.Session.SetString("usertype", usr.UserType);
+                HttpContext.Session.SetString("username", usr.Username);
+                if (usr.UserType == "admin")
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                return RedirectToAction("Index", "Home");
+                
+                
+            }
+            else
+            {
+                ViewBag.Error = "Please Enter Your Credentials.";
+                return View("Index");
+            }
+        }
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register([Bind("UserId,Username,Password,UserAddedDate,UserType")]  User user)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(user);
+                await context.SaveChangesAsync();
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ Models/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace coreSessionManagementApplication.Models
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=H5CG125CW45;Database=EcommerceDB;Integrated Security=true;");
+        }
+    }
+}
